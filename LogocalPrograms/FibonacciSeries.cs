6ca2ed4ed1f26e339322b3ca525c54cb36e751db
patelaftab <10patelaftab@gmail.com>
2022-12-19 12:52:14 +0530

    Checking a Fibonacci Series Program

---
+++ LogocalPrograms/FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogocalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void checknumber()
+        {
+            Console.WriteLine("Welcome To Fibonacci Series Program");
+            int n1 = 0, n2 = 1, n3, num;
+            Console.WriteLine("Enter Number to check Fibonacci series");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " \n" +
+                n2 + "");
+            for(int i=2; i<num; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3+ " " );
+                n1 = n2;
+                n2= n3;
+            }
+
+        }
+    }
+}
---
+++ LogocalPrograms/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogocalPrograms
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Logical Programs : Please Choose a Option\n" +
+                "Press 1: Fibonacci Series");
+            int options=Convert.ToInt32(Console.ReadLine());
+            switch(options) 
+            {
+                case 1:
+                    FibonacciSeries.checknumber();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
