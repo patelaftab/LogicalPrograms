a17ec8dcdb7c166842b85b551e8ea3417b9786c0
patelaftab <10patelaftab@gmail.com>
2022-12-19 14:22:55 +0530

    [Aftab] Checking Number is Perfect or Not

---
+++ LogocalPrograms/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogocalPrograms
+{
+    public class PerfectNumber
+    {
+        public static void checkingperfectnumber()
+        {
+            Console.WriteLine("Welcome To Perfect Number Program");
+            Console.WriteLine("Enter a initial Number range to Check Whether Perfect Number or Not");
+            int nx=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Ending Number Of range ");
+            int ny=Convert.ToInt32(Console.ReadLine());
+            int n, i, sum;
+            for (n = nx; n <= ny; n++)
+            {
+                i = 1;
+                sum= 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                    
+                        sum = sum + i;
+                        i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0} " ,n);
+            }
+        }
+    }
+}
--- LogocalPrograms/Program.cs
+++ LogocalPrograms/Program.cs
@@ -11,12 +11,19 @@ namespace LogocalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Logical Programs : Please Choose a Option\n" +
-                "Press 1: Fibonacci Series");
+                "Press 1: Fibonacci Series Program\n" +
+                "Press 2: Perfect Number Program");
             int options=Convert.ToInt32(Console.ReadLine());
             switch(options) 
             {
                 case 1:
                     FibonacciSeries.checknumber();
+                    break;
+                case 2:
+                    PerfectNumber.checkingperfectnumber();
+                    break;
+                default:
+                    Console.WriteLine("Please Chose a Valid Option");
                     break;
             }
             Console.ReadLine();
